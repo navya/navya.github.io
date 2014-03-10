a011bcaa5250cafcc69ee8dbc983f18a5b05adc7
rejuvyesh <mail@rejuvyesh.com>
2014-03-11 00:27:44 +0530

    stylize

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,19 @@
 -- File: Main.hs
 
-{-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE Arrows            #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
 
 module Main where
 
-import Hakyll
+import           Hakyll
 
 main ∷ IO ()
 main = hakyll $ do
   match ("images/*" .||. "js/*" .||. "favicon.ico" .||. "fonts/*") $ do
     route idRoute
     compile copyFileCompiler
-    
+
   match "css/*" $ do
     route idRoute
     compile compressCssCompiler
@@ -23,21 +23,24 @@ main = hakyll $ do
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
       >>= relativizeUrls
-      
+
   match (fromList pages) $ do
     route $ setExtension ".html"
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
       >>= relativizeUrls
       >>= removeIndexHtml
-    -- Read templates
+
+  -- Read templates
   match "templates/*" $ compile $ templateCompiler
+
   where
     pages =
       [  "about/index.md"
       ,  "people/index.md"
       ,  "services/index.md"
-      ,  "gallery/index.md"]
+      ,  "gallery/index.md"
+      ]
 
 removeIndexHtml ∷ Item String → Compiler (Item String)
 removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
@@ -46,6 +49,6 @@ removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
     removeIndexStr str@(x:xs) | str == "/index.html" = ""
                               | otherwise = x:removeIndexStr xs
     removeIndexStr [] = []
-      
 
-      
+
+
