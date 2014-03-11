54551b402a2b686ca5fb3f000583cf28e370ec1c
rejuvyesh <mail@rejuvyesh.com>
2014-03-11 14:56:11 +0530

    nicer pages, ugly hack though :/

--- src/Main.hs
+++ src/Main.hs
@@ -60,6 +60,6 @@ woptions ∷ WriterOptions
 woptions = defaultHakyllWriterOptions{ writerSectionDivs = True,
                                        writerStandalone  = True,
                                        writerColumns     = 120,
-                                       writerTemplate    = "<div id =\"content\">\n$body$\n</div>\n",
+                                       writerTemplate    = "<div id=\"text2\">About</div>\n<div id=\"text3\">Services</div>\n<div id=\"text4\">Gallery</div>\n<div id=\"text5\">Internal Wiki</div>\n<div id=\"text6\">People</div>\n<div id=\"text7\">Our Motto</div>\n<div id =\"content\">\n$body$\n</div>\n",
                                        writerHtml5       = True
                                      }
